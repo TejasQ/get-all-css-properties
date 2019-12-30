df791315ab565907913468aae4f6fbb8b0d4d406
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2019-12-31 00:44:56 +0100

    Getting all CSS styles more efficiently :)

--- src/getAllCSSProperties.ts
+++ src/getAllCSSProperties.ts
@@ -1,21 +1,7 @@
-import fetch from "node-fetch";
-
 export const getAllCSSProperties = () => {
-  const searchRegexp = new RegExp('<li><a href="/css/(.*)">(.*)</a></li>', "igm");
-  return fetch("https://cors-anywhere.herokuapp.com/https://www.quackit.com/css/properties/", {
-    headers: {
-      origin: "http://127.0.0.1",
-    },
-  })
-    .then(r => r.text())
-    .then(text => {
-      const accumulator: Set<string> = new Set(); // let's use a Set to avoid duplicates HOMIE
-      let match: RegExpExecArray | null;
-      while ((match = searchRegexp.exec(text))) {
-        accumulator.add(match[2]);
-      }
-      return Array.from(accumulator)
-        .sort()
-        .slice(2);
-    });
+  return Object.keys(document.createElement("div").style).map(prop =>
+    prop
+      .replace(/[\w]([A-Z])/g, match => match[0] + "-" + match[1])
+      .toLowerCase()
+  );
 };
